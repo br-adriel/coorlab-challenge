98fef717f406d4621e647d60eb33d925b473f8b3
Adriel <adriel.fsantos@outlook.com>
2024-03-19 20:26:03 -0300

    feat: Add route for travel_options_view

--- app/backend/server/travels/urls.py
+++ app/backend/server/travels/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import get_all_view, get_destinations_view
+from .views import get_all_view, get_destinations_view, travel_options_view
 
 urlpatterns = [
   path('', get_all_view, name="list"),
   path('destinations', get_destinations_view, name="destinations"),
+  path('options', travel_options_view, name="travel_options"),
 ]
