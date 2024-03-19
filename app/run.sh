d56cba23aede391537f1f1fb1c4856ee4e680195
Adriel <adriel.fsantos@outlook.com>
2024-03-19 20:45:40 -0300

    feat: Update run script to start django backend

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo "Implemente aqui o script para executar a sua solução"
+echo "DJANGO SETUP => STARTING..."
+
+echo "DJANGO SETUP: CREATING VIRTUAL ENVIRONMENT"
+cd backend
+python3 -m venv .venv
+source .venv/bin/activate
+
+echo "DJANGO SETUP: INSTALLING DEPENDENCIES"
+pip install -r requirements.txt
+
+echo "DJANGO SETUP: RUN SERVER IN BACKGROUND"
+cd server
+python3 manage.py runserver 3000 &
+
+echo "DJANGO SETUP => DONE"
+
+
+echo ""
+echo "RETURN TO INITIAL STATE => STARTING..."
+echo "RETURN TO INITIAL STATE: RETURN TO APP FOLDER"
+cd ../..
+echo "RETURN TO INITIAL STATE => DONE"
