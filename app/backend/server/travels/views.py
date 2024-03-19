74c4d4162ccdc44a33b9c8190265ccf1649a1110
Adriel <adriel.fsantos@outlook.com>
2024-03-19 13:37:10 -0300

    feat: Add get_destinations_view

--- app/backend/server/travels/views.py
+++ app/backend/server/travels/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Travel
 
 # Create your views here.
+@api_view()
+def get_destinations_view(request):
+    destinations = Travel.objects.values_list('city', flat=True)
+    destinations = list(dict.fromkeys(list(destinations)))
+    return Response(destinations)
+
