e34ce6d961656f4bae51b4b2fbee63f0f8ead0c9
Adriel <adriel.fsantos@outlook.com>
2024-03-19 20:25:51 -0300

    feat: Add travel_options_view

--- app/backend/server/travels/views.py
+++ app/backend/server/travels/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
@@ -21,3 +22,38 @@ def get_all_view(request):
     
     serializer = TravelSerializer(paginated_data, many=True)
     return paginator.get_paginated_response(serializer.data)
+
+@api_view(['post'])
+def travel_options_view(request):
+    # check if required data is passed
+    if any(field not in request.data.keys() for field in ['date', 'city']):
+        return Response(status=400)
+    
+    # parse date string sended on request to datetime type
+    # the travel date consider end of day for same day travel situation
+    parsed_date = request.data['date'].split('-')
+    travel_date = datetime(
+        int(parsed_date[0]), 
+        int(parsed_date[1]), 
+        int(parsed_date[2]), 
+        23, 59, 59
+    )
+    
+    today = datetime.now()
+    if travel_date < today:
+        return Response(status=400)
+    
+    hours_to_travel = travel_date  - today
+
+    query = Travel.objects.filter(
+        city__iexact=request.data['city'],
+        duration__lte=hours_to_travel
+    )
+    best_option = query.order_by('duration').first()
+    cheapest_option = query.order_by('price_econ').first()
+    
+    serialized_response = {
+        'best_option': None if best_option is None else TravelSerializer(best_option).data,
+        'cheapest_option': None if cheapest_option is None else TravelSerializer(cheapest_option).data
+    }
+    return Response(serialized_response)
