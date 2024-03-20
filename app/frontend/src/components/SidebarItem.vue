c5d44d23e580ac5afa5a10c3c3f58dc397181a13
Adriel <adriel.fsantos@outlook.com>
2024-03-20 19:45:33 -0300

    feat: Add SidebarItem component

---
+++ app/frontend/src/components/SidebarItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+const props = defineProps({
+  to: {
+    required: true,
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <RouterLink :to="props.to" class="text-white font-medium text-sm">
+    <div class="flex items-baseline gap-2">
+      <slot></slot>
+    </div>
+  </RouterLink>
+</template>
