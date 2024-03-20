e7be84a6496137dfe4dd6c422f9b72ebc400a3f8
Adriel <adriel.fsantos@outlook.com>
2024-03-20 19:45:47 -0300

    feat: Add AppSidebar component

---
+++ app/frontend/src/components/AppSidebar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { CalculatorOutlined } from '@ant-design/icons-vue';
+import { RouterLink } from 'vue-router';
+import SidebarItem from './SidebarItem.vue';
+</script>
+
+<template>
+  <div
+    class="bg-secondary shadow-deep lg:shadow-none w-full lg:min-h-[100dvh] px-5"
+  >
+    <div class="container mx-auto">
+      <RouterLink to="/" class="block py-5">
+        <img
+          src="@/assets/img/logo.png"
+          alt="Logo da CB Viagens"
+          width="160px"
+          class="max-w-full"
+        />
+      </RouterLink>
+
+      <nav class="hidden lg:flex flex-col gap-4 mt-6">
+        <SidebarItem to="/">
+          <CalculatorOutlined class="text-lg" />
+          Calculadora de Viagem
+        </SidebarItem>
+      </nav>
+    </div>
+  </div>
+</template>
