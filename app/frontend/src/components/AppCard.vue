ce33e6803b72a5213b5e9a0d1e75ac12923d3d02
Adriel <adriel.fsantos@outlook.com>
2024-03-20 20:30:40 -0300

    feat: Add AppCard component

---
+++ app/frontend/src/components/AppCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  showHeader: {
+    default: true,
+    required: false,
+    type: Boolean,
+  },
+});
+</script>
+
+<template>
+  <div class="bg-plain-lighter w-full rounded shadow-lg">
+    <div
+      class="bg-secondary px-8 py-4 rounded-t text-white font-semibold"
+      v-if="props.showHeader"
+    >
+      <slot name="header"></slot>
+    </div>
+    <div class="p-4"><slot></slot></div>
+  </div>
+</template>
