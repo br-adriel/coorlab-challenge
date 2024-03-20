4ea56845e6890d7bb5a6c08d44836dbf17c4654b
Adriel <adriel.fsantos@outlook.com>
2024-03-20 19:46:29 -0300

    feat: Setup App layout

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import AppSidebar from '@/components/AppSidebar.vue';
 </script>
 
 <template>
-  <RouterView />
+  <div class="lg:flex">
+    <div class="lg:w-1/5">
+      <AppSidebar />
+    </div>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
