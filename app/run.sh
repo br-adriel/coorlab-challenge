d2cd5974f73f98e8b78a06210a6ae99d99968c69
Adriel <adriel.fsantos@outlook.com>
2024-03-21 19:20:27 -0300

    feat: Update run script with frontend project instructions

--- app/run.sh
+++ app/run.sh
@@ -8,11 +8,12 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 echo "DJANGO SETUP: INSTALLING DEPENDENCIES"
-pip install -r requirements.txt
+pip install -r requirements.txt &>/dev/null
 
 echo "DJANGO SETUP: RUN SERVER IN BACKGROUND"
 cd server
-python3 manage.py runserver 3000 &
+python3 manage.py runserver 3000 &>/dev/null &
+echo "DJANGO SETUP => PID: $!"
 
 echo "DJANGO SETUP => DONE"
 
@@ -22,3 +23,37 @@ echo "RETURN TO INITIAL STATE => STARTING..."
 echo "RETURN TO INITIAL STATE: RETURN TO APP FOLDER"
 cd ../..
 echo "RETURN TO INITIAL STATE => DONE"
+echo ""
+
+
+echo "VUE SETUP => STARTING..."
+
+echo "VUE SETUP: INSTALLING DEPENDENCIES"
+cd frontend
+npm i &>/dev/null
+
+echo "VUE SETUP: BUILD PROJECT"
+npm run build &>/dev/null
+
+echo "VUE SETUP: RUN SERVER IN BACKGROUND"
+npm run preview &>/dev/null &
+echo "VUE SETUP => PID: $!"
+
+echo "VUE SETUP => DONE"
+
+echo ""
+echo "RETURN TO INITIAL STATE => STARTING..."
+echo "RETURN TO INITIAL STATE: RETURN TO APP FOLDER"
+cd ..
+echo "RETURN TO INITIAL STATE => DONE"
+echo ""
+
+echo "=== SUCCESS - SETUP CONCLUDED ==="
+echo ""
+echo "FRONTEND >> http://localhost:8080"
+echo "BACKEND >> http://localhost:3000/travels"
+echo ""
+echo "SUBMISSION BY ADRIEL SANTOS"
+echo "https://github.com/br-adriel"
+echo ""
+echo "================================="
