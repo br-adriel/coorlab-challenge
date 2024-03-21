1706dc54f7113b6c1e202f26eff40cc7680507f2
Adriel <adriel.fsantos@outlook.com>
2024-03-21 16:53:35 -0300

    feat: Add TravelOptions component

---
+++ app/frontend/src/components/TravelDisplay/TravelOptions.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TravelInfo from './TravelInfo.vue';
+
+const props = defineProps({
+  best: Object,
+  cheapest: Object,
+});
+</script>
+
+<template>
+  <div class="w-full">
+    <p class="w-2/3 mb-3">
+      Essas são as melhores alternativas de viagem para a data selecionada
+    </p>
+    <TravelInfo :travel="best" class="mb-3" />
+    <TravelInfo :travel="cheapest" :cheapest-option="true" />
+  </div>
+</template>
