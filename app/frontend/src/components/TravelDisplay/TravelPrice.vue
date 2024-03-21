e4915cd987b0f6bd2e247ebaad16636bbfd53d7a
Adriel <adriel.fsantos@outlook.com>
2024-03-21 16:53:07 -0300

    feat: Add TravelPrice component

---
+++ app/frontend/src/components/TravelDisplay/TravelPrice.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { moneyFormatter } from '@/utils/intl';
+import clsx from 'clsx';
+
+const props = defineProps({
+  darkerVariant: Boolean,
+  price: Number,
+});
+</script>
+
+<template>
+  <div
+    :class="
+      clsx({
+        'rounded w-1/3 flex flex-col justify-center p-4 text-sm': true,
+        'bg-travel-lighter': !darkerVariant,
+        'bg-travel-darker': darkerVariant,
+      })
+    "
+  >
+    <p class="font-bold">Preço</p>
+    <p>
+      {{ moneyFormatter.format(price) }}
+    </p>
+  </div>
+</template>
