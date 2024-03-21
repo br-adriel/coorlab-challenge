23bf137bc6b33ddd4c802e18f32dd4aa98b160e5
Adriel <adriel.fsantos@outlook.com>
2024-03-21 16:52:54 -0300

    feat: Add TravelIcon component

---
+++ app/frontend/src/components/TravelDisplay/TravelIcon.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { PhClock, PhHandCoins } from '@phosphor-icons/vue';
+
+const props = defineProps({
+  cheapestOption: Boolean,
+});
+</script>
+
+<template>
+  <div
+    class="bg-primary px-4 py-6 rounded-l text-white h-full flex items-center"
+  >
+    <PhClock size="32px" v-if="props.cheapestOption" />
+    <PhHandCoins size="32px" v-else />
+  </div>
+</template>
