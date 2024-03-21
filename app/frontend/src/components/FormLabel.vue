e51768b4f3e50cbf246f5bec0b673830085378b0
Adriel <adriel.fsantos@outlook.com>
2024-03-21 12:35:03 -0300

    feat: Add FormLabel component

---
+++ app/frontend/src/components/FormLabel.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps({
+  label: String,
+  for: String,
+});
+</script>
+
+<template>
+  <label :for="props.for" class="font-bold text-sm">{{ props.label }}</label>
+</template>
