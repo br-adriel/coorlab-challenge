4ee7a3a57b7187fd1df67762536df6ca8ed9fe71
Adriel <adriel.fsantos@outlook.com>
2024-03-21 17:34:48 -0300

    feat: Add Modal component

---
+++ app/frontend/src/components/Modal.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppCard from '@/components/AppCard.vue';
+
+const emit = defineEmits(['modalClose']);
+</script>
+
+<template>
+  <div
+    class="bg-gray-500 bg-opacity-50 fixed h-full w-full overflow-y-auto overflow-x-hidden top-0 left-0 p-5 z-50"
+    @click="$emit('modalClose')"
+  >
+    <div class="flex items-center justify-center min-h-full">
+      <AppCard :show-header="false" class="w-auto">
+        <slot></slot>
+      </AppCard>
+    </div>
+  </div>
+</template>
